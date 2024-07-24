22ff2c5bb16c15931deebd44bd2b498af7f0d05a
abolfazl <abolfazlkameli0@gmail.com>
2024-07-24 17:49:00 +0330

    project: add IPaddress and request time to info.

--- tracking/base_mixins.py
+++ tracking/base_mixins.py
@@ -1,6 +1,37 @@
+import ipaddress
+
+from django.utils.timezone import now
+
+
 class BaseLoggingMixin:
     def initial(self, request, *args, **kwargs):
+        self.info = {'requested_at': now()}
         return super().initial(request, *args, **kwargs)
 
     def finalize_response(self, request, *args, **kwargs):
-        return super().finalize_response(request, *args, **kwargs)
+        response = super().finalize_response(request, *args, **kwargs)
+        self.info.update({
+            'remote_addr': self._get_ip_address(request),
+        })
+        self.handle_info()
+        return response
+
+    def handle_info(self):
+        raise NotImplementedError
+
+    def _get_ip_address(self, request):
+        ipaddr = request.META.get('HTTP_X_FORWARDED_FOR')
+        if ipaddr:
+            ipaddr = ipaddr.split(',')[0]
+        else:
+            ipaddr = request.META.get('REMOTE_ADDR').split(',')[0]
+
+        possibles = (ipaddr.lstrip('[').split(']')[0], ipaddr.split(':')[0])
+
+        for addr in possibles:
+            try:
+                return str(ipaddress.ip_address(addr))
+            except:
+                pass
+
+        return ipaddr
--- tracking/mixins.py
+++ tracking/mixins.py
@@ -2,4 +2,6 @@ from .base_mixins import BaseLoggingMixin
 
 
 class LoggingMixin(BaseLoggingMixin):
-    ...
+    def handle_info(self):
+        # APIRequestLog(**self.info).save()
+        print(self.info)
