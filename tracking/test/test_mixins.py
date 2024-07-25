b954dc7ebc0c12afbf83f8a813def887fada04a0
abolfazl <abolfazlkameli0@gmail.com>
2024-07-25 21:02:35 +0330

    fix: using override_settings.

--- tracking/test/test_mixins.py
+++ tracking/test/test_mixins.py
@@ -1,14 +1,16 @@
-from django.urls import reverse
-from rest_framework.test import APITestCase
+from django.test import override_settings
+from rest_framework.test import APITestCase, APIRequestFactory
 
 from tracking.models import APIRequestLog
+from . import views
 
 
+@override_settings(ROOT_URLCONF='tracking.test.urls')
 class TestLoggingMixin(APITestCase):
     def test_no_logging(self):
-        self.client.get(reverse('tracking:no-logging'))
+        self.client.get('/no-logging/')
         self.assertEqual(APIRequestLog.objects.all().count(), 0)
 
     def test_logging(self):
-        self.client.get(reverse('tracking:logging'))
+        self.client.get('/logging/')
         self.assertEqual(APIRequestLog.objects.all().count(), 1)
--- tracking/urls.py
+++ tracking/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
 app_name = 'tracking'
 urlpatterns = [
     path('', views.HomeAPI.as_view(), name='home'),
-    path('test/', include('tracking.test.urls')),
 ]
