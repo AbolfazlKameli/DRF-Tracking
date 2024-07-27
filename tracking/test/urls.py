ceb2763728a89d3748af0e644c36a4e57c2242ab
abolfazl <abolfazlkameli0@gmail.com>
2024-07-27 16:19:59 +0330

    test: add new views.

--- tracking/test/urls.py
+++ tracking/test/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from . import views
+from . import views as test_views
 
 urlpatterns = [
-    path('no-logging/', views.MockNoLoggingAPI.as_view(), name='no-logging'),
-    path('logging/', views.MockLoggingAPI.as_view(), name='logging'),
+    path('no-logging/', test_views.MockNoLoggingAPI.as_view(), name='no-logging'),
+    path('logging/', test_views.MockLoggingAPI.as_view(), name='logging'),
+    path('explicit-logging/', test_views.MockExplicitLoggingAPI.as_view(), name='explicit-logging'),
+    path('custom-check-logging/', test_views.MockCustomCheckLoggingAPI.as_view(), name='custom-check-logging'),
+    path('session-auth-logging/', test_views.MockSessionAuthLoggingAPI.as_view(), name='session-auth-logging'),
+    path('token-auth-logging/', test_views.MockTokenAuthLoggingAPI.as_view(), name='token-auth-logging'),
+    path('sensitive-fields-logging/', test_views.MockSensitiveFieldsLoggingAPI.as_view(),
+         name='sensitive-fields-logging'),
+    path('invalid-cleaned-logging/', test_views.MockInvalidCleanedSubstituteLoggingAPI.as_view(),
+         name='invalid-cleaned-logging'),
 ]
--- tracking/test/views.py
+++ tracking/test/views.py
@@ -1,3 +1,5 @@
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,3 +14,51 @@ class MockNoLoggingAPI(APIView):
 class MockLoggingAPI(LoggingMixin, APIView):
     def get(self, request):
         return Response({'message': 'logging'})
+
+
+class MockExplicitLoggingAPI(LoggingMixin, APIView):
+    logging_methods = ['POST']
+
+    def get(self, request):
+        return Response({'message': 'no logging'})
+
+    def post(self, request):
+        return Response({'message': 'logging'})
+
+
+class MockCustomCheckLoggingAPI(LoggingMixin, APIView):
+    def should_log(self, request, response):
+        return 'log' in response.data
+
+    def get(self, request):
+        return Response({'message': 'logging'})
+
+    def post(self, request):
+        return Response({'message': 'no recording'})
+
+
+class MockSessionAuthLoggingAPI(LoggingMixin, APIView):
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response({'message': 'session auth logging'})
+
+
+class MockTokenAuthLoggingAPI(LoggingMixin, APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response({'message': 'token auth logging'})
+
+
+class MockSensitiveFieldsLoggingAPI(LoggingMixin, APIView):
+    sensitive_fields = {'mYfiEld'}
+
+    def get(self, request):
+        return Response({'message': 'logging'})
+
+
+class MockInvalidCleanedSubstituteLoggingAPI(LoggingMixin, APIView):
+    CLEANED_SUBSTITUTE = 1234
